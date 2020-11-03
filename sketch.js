0daeb91ec7fa26a5257f5e188f0337f79a614a26
cheryl1908 <73298909+cheryl1908@users.noreply.github.com>
2020-11-03 15:54:34 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -8,7 +8,7 @@ function setup() {
   weight=random(400,1500);
   wall=createSprite(1500,200,60,height/2);
   car=createSprite(50,200,50,50);
-
+}
 function draw() {
   background(0); 
   
@@ -18,15 +18,14 @@ function draw() {
     car.velocityX=0;
     var deformation=weight*speed*speed/22500;
     if(deformation>180){
-      car.shapeColor(255,0,0);
+      car.shapeColor=rgb(255,0,0);
     }
     if(deformation<180 && deformation>100){
-      car.shapeColor(230,230,0);
+      car.shapeColor=rgb(230,230,0);
     }
     if(deformation<100){
-      car.shapeColor(0,255,0);  
+      car.shapeColor=rgb(0,255,0);  
     }
   }
   drawSprites();
-}
 }
