0ee1c2bdb5d23da8ec61bd09a0aa1bce21c93272
cheryl1908 <73298909+cheryl1908@users.noreply.github.com>
2020-11-04 18:24:54 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ function setup() {
 
   speed=random(55,90);
   weight=random(400,1500);
-  wall=createSprite(1500,200,60,height/2);
+  wall=createSprite(600,200,60,height/2);
   car=createSprite(50,200,50,50);
 }
 function draw() {
